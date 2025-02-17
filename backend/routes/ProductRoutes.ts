af459cdd362d94c3ff2a57aa0208f9b4dd0ff401
ethschan <chan.ethan@gmail.com>
2025-02-16 18:05:07 -0800

    get products route and database deployment

--- backend/routes/ProductRoutes.ts
+++ backend/routes/ProductRoutes.ts
@@ -6,35 +6,10 @@ const controller = new ProductController()
 export const ProductRoutes = [
     {
         method: "get",
-        route: "/products",
-        action: controller.getProducts,
-        validation: []
-    },
-    {
-        method: "post",
-        route: "/products",
-        action: controller.postProducts,
+        route: "/product/:id",
+        action: controller.getProductById,
         validation: [
-            body("task").isString(),
-            body("urgent").isBoolean().optional()
-        ]
-    },
-    {
-        method: "put",
-        route: "/products/:id",
-        action: controller.putProducts,
-        validation: [
-            param("id").isMongoId(),
-            body("task").isString(),
-            body("urgent").isBoolean().optional()
-        ]
-    },
-    {
-        method: "delete",
-        route: "/products/:id",
-        action: controller.deleteProducts,
-        validation: [
-            param("id").isMongoId()
+            param('id').isString().withMessage('Invalid product ID')
         ]
     }
 ]
