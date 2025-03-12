f9a927cd743efa33c4b0bac94bee230ded79170a
ethschan <chan.ethan@gmail.com>
2025-03-12 14:46:14 -0700

    this is fun

--- backend/services.ts
+++ backend/services.ts
@@ -2,16 +2,15 @@ import { MongoClient } from 'mongodb';
 import { initializeApp, getApps, App } from 'firebase-admin/app';
 import { OAuth2Client } from 'google-auth-library';
 
-export const client: MongoClient = (new MongoClient(process.env.DB_URI as string ?? "mongodb://localhost:27017")) as MongoClient;
-export const userCollection = client.db("users_db").collection("users");
+export const client: MongoClient = new MongoClient(process.env.DB_URI as string ?? "mongodb://localhost:27017");
 
 let firebase_app: App;
 
 export function getFirebaseApp() {
-    if (!getApps().length) {
+    if (!getApps()) {
         firebase_app = initializeApp();
     }
     return firebase_app;
 }
 
-export const oauthClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+export const oauthClient: OAuth2Client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
