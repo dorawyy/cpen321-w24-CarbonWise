004af9634a38eeadb9011ef935b6ecf657e412cf
ethschan <chan.ethan@gmail.com>
2025-03-12 13:09:22 -0700

    updating jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,10 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/dist/**',
+    '!**/tests/**',
+    '!jest.config.js',
+  ],
   testEnvironment: "node",
   testMatch: ['**/**/*.test.ts'],
   forceExit: true,
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
-  },
+  }
 };
